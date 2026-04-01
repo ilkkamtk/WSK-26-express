ddc5245f6f3a5666535c9575793d1469b2a1e924
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2026-04-01 11:45:59 +0300

    refactor: convert user model functions to async/await and implement modifyUser and removeUser

--- src/api/controllers/user-controller.js
+++ src/api/controllers/user-controller.js
@@ -1,11 +1,17 @@
-import {addUser, findUserById, listAllUsers} from '../models/user-model.js';
+import {
+  addUser,
+  findUserById,
+  listAllUsers,
+  modifyUser,
+  removeUser,
+} from '../models/user-model.js';
 
-const getUser = (req, res) => {
-  res.json(listAllUsers());
+const getUser = async (req, res) => {
+  res.json(await listAllUsers());
 };
 
-const getUserById = (req, res) => {
-  const user = findUserById(req.params.id);
+const getUserById = async (req, res) => {
+  const user = await findUserById(req.params.id);
   if (user) {
     res.json(user);
   } else {
@@ -13,8 +19,8 @@ const getUserById = (req, res) => {
   }
 };
 
-const postUser = (req, res) => {
-  const result = addUser(req.body);
+const postUser = async (req, res) => {
+  const result = await addUser(req.body);
   if (result.user_id) {
     res.status(201);
     res.json({message: 'New user added.', result});
@@ -26,16 +32,22 @@ const postUser = (req, res) => {
 // PUT /api/v1/users/:id - return hard coded json response:
 // {message: 'user item updated.'}
 
-const putUser = (req, res) => {
-  // not implemented in this example, this is homework
-  //res.sendStatus(200);
-  res.json({message: 'user item updated.'});
+const putUser = async (req, res) => {
+  const result = await modifyUser(req.body, req.params.id);
+  if (result.message === 'success') {
+    res.json({message: 'user item updated.'});
+  } else {
+    res.sendStatus(400);
+  }
 };
 
-const deleteUser = (req, res) => {
-  // not implemented in this example, this is homework
-  //res.sendStatus(200);
-  res.json({message: 'user item deleted.'});
+const deleteUser = async (req, res) => {
+  const result = await removeUser(req.params.id);
+  if (result.message === 'success') {
+    res.json({message: 'user item deleted.'});
+  } else {
+    res.sendStatus(400);
+  }
 };
 
 export {getUser, getUserById, postUser, putUser, deleteUser};
--- src/api/models/user-model.js
+++ src/api/models/user-model.js
@@ -1,35 +1,59 @@
-// mock data
-const userItems = [
-  {
-    user_id: 3609,
-    name: 'John Doe',
-    username: 'johndoe',
-    email: 'john@metropolia.fi',
-    role: 'user',
-    password: 'password',
-  },
-];
+import promisePool from '../../utils/database.js';
 
-const listAllUsers = () => {
-  return userItems;
+const listAllUsers = async () => {
+  const [rows] = await promisePool.query('SELECT * FROM wsk_users');
+  // console.log('rows', rows);
+  return rows;
 };
 
-const findUserById = (id) => {
-  return userItems.find((item) => item.user_id == id);
+const findUserById = async (id) => {
+  const [rows] = await promisePool.execute(
+    'SELECT * FROM wsk_users WHERE user_id = ?',
+    [id]
+  );
+  // console.log('rows', rows);
+  if (rows.length === 0) {
+    return false;
+  }
+  return rows[0];
 };
 
-const addUser = (user) => {
-  const {name, username, email, role, password} = user;
-  const newId = userItems[0].user_id + 1;
-  userItems.unshift({
-    user_id: newId,
-    username,
-    name,
-    email,
-    role,
-    password,
-  });
-  return {user_id: newId};
+const addUser = async (user) => {
+  const {username, email, name, password} = user;
+  const sql = `INSERT INTO wsk_users (username, email, name, password)
+               VALUES (?, ?, ?, ?)`;
+  const params = [username, email, name, password];
+  const [result] = await promisePool.execute(sql, params);
+  console.log('result', result);
+  if (result.affectedRows === 0) {
+    return false;
+  }
+  return {user_id: result.insertId};
 };
 
-export {listAllUsers, findUserById, addUser};
+const modifyUser = async (user, id) => {
+  const sql = promisePool.format(`UPDATE wsk_users SET ? WHERE user_id = ?`, [
+    user,
+    id,
+  ]);
+  const [result] = await promisePool.execute(sql);
+  console.log('result', result);
+  if (result.affectedRows === 0) {
+    return false;
+  }
+  return {message: 'success'};
+};
+
+const removeUser = async (id) => {
+  const [rows] = await promisePool.execute(
+    'DELETE FROM wsk_users WHERE user_id = ?',
+    [id]
+  );
+  console.log('rows', rows);
+  if (rows.affectedRows === 0) {
+    return false;
+  }
+  return {message: 'success'};
+};
+
+export {listAllUsers, findUserById, addUser, modifyUser, removeUser};
