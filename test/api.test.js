e12925f0c7803592945f182af22bb07251d7286a
ilkkamtk <ilkka.kylmaniemi@metropolia.fi>
2026-04-27 15:34:55 +0300

    feat: update user registration test with dynamic username and email generation

--- test/api.test.js
+++ test/api.test.js
@@ -1,15 +1,19 @@
 import app from '../src/app';
 import {registerUser} from './testUser';
+// randomstring kirjasto, tee AI:llä
+
+let token = '';
+const random = randomstring(7);
 
 describe('User related tests', () => {
+  const user = {
+    username: 'Cyrö' + random,
+    name: 'Juha',
+    email: `juha.${random}@metropolia.fi`,
+    role: 'teacher / failed comedian',
+    password: 'tähän oman pankin salasana',
+  };
   it('registers a new user', async () => {
-    const user = {
-      username: 'Cyrö',
-      name: 'Juha',
-      email: 'juha.tauriainen@metropolia.fi',
-      role: 'teacher / failed comedian',
-      password: 'tähän oman pankin salasana',
-    };
     await registerUser(app, '/api/v1/users', user);
   });
 });
